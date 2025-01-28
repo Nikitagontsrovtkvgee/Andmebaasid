796f26a5072e5507bcca8b277c02d77e2bba9e44
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2025-01-28 15:35:18 +0200

    Opilane.sql

--- opilane.sql
+++ opilane.sql
@@ -59,3 +59,39 @@ select * from opilane;
 --andmete uuendamine
 UPDATE opilane SET aadress='Tartu'
 WHERE opilaneID=3
+
+CREATE TABLE Language
+(
+ID int NOT NULL PRIMARY KEY,
+Code char(3) NOT NULL,
+Language varchar(50) NOT NULL,
+IsOfficial bit,
+Percentage smallint,
+);
+SElect * from Language
+
+INSERT INTO Language(ID, Code, Language)
+Values(1, 'EST', 'Eesti'), (2, 'RUS', 'Vene'),
+(3, 'ENG', 'Inglise'), (4, 'G', 'Saksa')
+
+CREATE TABLE keeleValik(
+keeleValikID int primary key identity(1,1),
+valikuNimetus varchar(10) not null,
+opilaneID int,
+Foreign key (opilaneID) references opilane(opilaneID),
+Language int,
+Foreign key (Language) references Language(ID)
+)
+select * from keelevalik;
+select * from Language;
+select * from opilane;
+
+INSERT INTO keelevalik(valikuNimetus, opilaneID, Language)
+Values ('valik A', 1, 1),
+('valik B', 2, 3),
+('valik A', 3, 2)
+
+SELECT *
+FROM opilane, Language, keelevalik
+WHERE opilane.opilaneID=keelevalik.opilaneID
+AND Language.ID=keelevalik.Language
