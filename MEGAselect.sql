f900b889b3d181770d2b37fb2132c4ed9a2c70e6
Nikita Gontsarov TARpv24 <78666039+Nikitagontsrovtkvgee@users.noreply.github.com>
2025-02-21 20:42:29 +0200

    MEGAselect.sql

---
+++ MEGAselect.sql
@@ -0,0 +1,187 @@
+create database koolGontsarov;
+use koolGontsarov;
+
+create table Mr.Bist (
+	opilaneID INT,
+	eesnimi VARCHAR(50),
+	perenimi VARCHAR(50),
+	epost VARCHAR(50),
+	sugu VARCHAR(50),
+	stip INT
+);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (1, 'Arne', 'Edmundson', 'aedmundson0@reference.com', 'Male', 68);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (2, 'Whitney', 'Fothergill', 'wfothergill1@pbs.org', 'Male', 42);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (3, 'Ronnica', 'Gander', 'rgander2@whitehouse.gov', 'Female', 99);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (4, 'Hamish', 'Swaysland', 'hswaysland3@cmu.edu', 'Non-binary', 98);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (5, 'Barbabra', 'Mirams', 'bmirams4@bizjournals.com', 'Female', 13);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (6, 'Patrica', 'Chelnam', 'pchelnam5@nifty.com', 'Female', 78);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (7, 'Agosto', 'Meegin', 'ameegin6@si.edu', 'Male', 59);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (8, 'Robinet', 'Swatland', 'rswatland7@nature.com', 'Female', 93);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (9, 'Jenny', 'Somes', 'jsomes8@t.co', 'Female', 32);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (10, 'Leonora', 'Ailward', 'lailward9@webmd.com', 'Female', 51);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (11, 'Devina', 'McGlade', 'dmcgladea@shinystat.com', 'Female', 50);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (12, 'Desmond', 'Risley', 'drisleyb@sciencedaily.com', 'Bigender', 16);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (13, 'Ashlin', 'Schonfeld', 'aschonfeldc@google.nl', 'Male', 69);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (14, 'Giulia', 'Schriren', 'gschrirend@plala.or.jp', 'Female', 18);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (15, 'Adi', 'Pechard', 'apecharde@senate.gov', 'Female', 41);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (16, 'Amity', 'Couzens', 'acouzensf@e-recht24.de', 'Bigender', 14);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (17, 'Ozzy', 'Tolossi', 'otolossig@yelp.com', 'Male', 90);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (18, 'Warner', 'Starling', 'wstarlingh@1688.com', 'Male', 70);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (19, 'Yolanthe', 'Soulsby', 'ysoulsbyi@ibm.com', 'Polygender', 8);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (20, 'Tamarah', 'Botterman', 'tbottermanj@umich.edu', 'Female', 33);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (21, 'Brittni', 'Anthoney', 'banthoneyk@bravesites.com', 'Bigender', 11);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (22, 'Alphonse', 'Lillie', 'alilliel@amazon.com', 'Male', 87);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (23, 'Livvy', 'Stockill', 'lstockillm@cyberchimps.com', 'Female', 94);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (24, 'Artemus', 'Berthouloume', 'aberthouloumen@businessinsider.com', 'Male', 88);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (25, 'Dev', 'Bernet', 'dberneto@ed.gov', 'Male', 37);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (26, 'Delores', 'Friman', 'dfrimanp@diigo.com', 'Female', 97);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (27, 'Gale', 'Duerden', 'gduerdenq@samsung.com', 'Female', 29);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (28, 'Brendan', 'Beretta', 'bberettar@reference.com', 'Male', 56);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (29, 'Trudey', 'Doogan', 'tdoogans@statcounter.com', 'Female', 100);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (30, 'Melinde', 'Beeze', 'mbeezet@acquirethisname.com', 'Female', 62);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (31, 'Paola', 'Solomonides', 'psolomonidesu@cam.ac.uk', 'Female', 85);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (32, 'Raimundo', 'Southerden', 'rsoutherdenv@sitemeter.com', 'Male', 82);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (33, 'Olwen', 'Scandrett', 'oscandrettw@alibaba.com', 'Bigender', 27);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (34, 'Ashlen', 'Damrel', 'adamrelx@bandcamp.com', 'Female', 85);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (35, 'Sawyere', 'Loddy', 'sloddyy@mac.com', 'Male', 18);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (36, 'Rollo', 'Motto', 'rmottoz@skyrock.com', 'Male', 55);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (37, 'Sigfrid', 'Glaserman', 'sglaserman10@nifty.com', 'Male', 6);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (38, 'Penelopa', 'Landa', 'planda11@mit.edu', 'Female', 35);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (39, 'Maxine', 'Bulbrook', 'mbulbrook12@illinois.edu', 'Female', 50);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (40, 'Albrecht', 'Rubee', 'arubee13@cocolog-nifty.com', 'Male', 67);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (41, 'Feodor', 'Synan', 'fsynan14@noaa.gov', 'Male', 8);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (42, 'Merilyn', 'Hillaby', 'mhillaby15@lycos.com', 'Female', 79);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (43, 'Eugine', 'Mounch', 'emounch16@tinyurl.com', 'Female', 45);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (44, 'Felita', 'Cabedo', 'fcabedo17@prlog.org', 'Female', 34);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (45, 'Inesita', 'Emmett', 'iemmett18@ycombinator.com', 'Female', 33);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (46, 'Dion', 'Soares', 'dsoares19@rakuten.co.jp', 'Male', 23);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (47, 'Emlyn', 'Kleis', 'ekleis1a@ezinearticles.com', 'Polygender', 27);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (48, 'Iona', 'Halfpenny', 'ihalfpenny1b@studiopress.com', 'Female', 81);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (49, 'Rosalind', 'Cardis', 'rcardis1c@noaa.gov', 'Female', 79);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (50, 'Renato', 'Tukesby', 'rtukesby1d@mapy.cz', 'Male', 83);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (51, 'Doretta', 'Gong', 'dgong1e@google.ca', 'Female', 78);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (52, 'Welby', 'Copo', 'wcopo1f@nature.com', 'Male', 5);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (53, 'Arnuad', 'Brantl', 'abrantl1g@ycombinator.com', 'Male', 39);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (54, 'Vivie', 'Chambers', 'vchambers1h@csmonitor.com', 'Female', 20);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (55, 'Nari', 'Sego', 'nsego1i@senate.gov', 'Female', 61);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (56, 'Lexi', 'Coghlin', 'lcoghlin1j@miitbeian.gov.cn', 'Agender', 84);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (57, 'Karisa', 'Penn', 'kpenn1k@soup.io', 'Female', 33);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (58, 'Cristen', 'Hatchette', 'chatchette1l@prnewswire.com', 'Female', 31);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (59, 'Allx', 'Ganny', 'aganny1m@wufoo.com', 'Female', 89);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (60, 'Conway', 'Surgeon', 'csurgeon1n@ucsd.edu', 'Male', 77);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (61, 'Kerk', 'Yerrill', 'kyerrill1o@paginegialle.it', 'Male', 58);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (62, 'Augusta', 'Avrahamof', 'aavrahamof1p@yellowpages.com', 'Female', 32);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (63, 'Sinclare', 'Shier', 'sshier1q@unc.edu', 'Male', 43);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (64, 'Fifi', 'Gallant', 'fgallant1r@nydailynews.com', 'Female', 25);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (65, 'Rochella', 'Benz', 'rbenz1s@berkeley.edu', 'Agender', 56);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (66, 'Raviv', 'McInerney', 'rmcinerney1t@myspace.com', 'Male', 32);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (67, 'Enrika', 'Paxforde', 'epaxforde1u@nytimes.com', 'Female', 11);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (68, 'Yanaton', 'Lanney', 'ylanney1v@ca.gov', 'Male', 58);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (69, 'Kerstin', 'Hedan', 'khedan1w@google.cn', 'Female', 57);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (70, 'Bevin', 'Iacabucci', 'biacabucci1x@sogou.com', 'Male', 43);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (71, 'Nelly', 'Escoffrey', 'nescoffrey1y@dailymail.co.uk', 'Female', 62);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (72, 'Raimondo', 'Dumelow', 'rdumelow1z@uol.com.br', 'Male', 97);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (73, 'Sim', 'Chaman', 'schaman20@nhs.uk', 'Polygender', 93);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (74, 'Willie', 'Esby', 'wesby21@themeforest.net', 'Female', 80);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (75, 'Ethelind', 'Negus', 'enegus22@europa.eu', 'Genderqueer', 39);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (76, 'Kasey', 'Scogings', 'kscogings23@yahoo.com', 'Female', 77);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (77, 'Bondy', 'Bingham', 'bbingham24@sogou.com', 'Male', 38);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (78, 'Oralee', 'Acres', 'oacres25@sbwire.com', 'Female', 8);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (79, 'Nickie', 'Alesin', 'nalesin26@blinklist.com', 'Male', 93);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (80, 'Winn', 'Guillou', 'wguillou27@artisteer.com', 'Male', 38);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (81, 'Cissy', 'O''Siaghail', 'cosiaghail28@cpanel.net', 'Female', 98);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (82, 'Alair', 'Edgeley', 'aedgeley29@tuttocitta.it', 'Male', 54);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (83, 'Gabriello', 'Creany', 'gcreany2a@dyndns.org', 'Male', 31);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (84, 'Shannan', 'Nouch', 'snouch2b@desdev.cn', 'Male', 47);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (85, 'Darb', 'Van Hesteren', 'dvanhesteren2c@smugmug.com', 'Male', 79);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (86, 'Bryan', 'Lynn', 'blynn2d@bing.com', 'Male', 95);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (87, 'Howard', 'Veltman', 'hveltman2e@bloglines.com', 'Male', 82);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (88, 'Mirella', 'Pusill', 'mpusill2f@latimes.com', 'Female', 63);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (89, 'Archaimbaud', 'Rundall', 'arundall2g@drupal.org', 'Male', 90);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (90, 'Marcile', 'Connolly', 'mconnolly2h@wiley.com', 'Female', 20);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (91, 'Ciro', 'Tomei', 'ctomei2i@webnode.com', 'Male', 14);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (92, 'Bird', 'Atheis', 'batheis2j@amazon.com', 'Female', 12);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (93, 'Ange', 'Mulrean', 'amulrean2k@ucoz.com', 'Female', 4);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (94, 'Kristin', 'Dunnan', 'kdunnan2l@liveinternet.ru', 'Female', 71);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (95, 'Myrtle', 'Allender', 'mallender2m@reference.com', 'Female', 49);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (96, 'Nate', 'Alan', 'nalan2n@ftc.gov', 'Male', 41);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (97, 'Hilton', 'Swoffer', 'hswoffer2o@admin.ch', 'Male', 86);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (98, 'Skip', 'Thorius', 'sthorius2p@hugedomains.com', 'Male', 15);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (99, 'Emery', 'Markovic', 'emarkovic2q@bloglines.com', 'Male', 59);
+insert into Mr.Bist (opilaneID, eesnimi, perenimi, epost, sugu, stip) values (100, 'Kaleena', 'Varren', 'kvarren2r@epa.gov', 'Female', 86);
+
+select * from opilane;
+--kuva ainult kaks veergu tabelist opilane
+select perenimi, stip from opilane;
+--kuva kolm esimest veergu ja sorteeri andmed stip järgi
+--kasvavas järgikorras - возрастание - väiksemast suurimani, A-->Z
+select opilaneID, eesnimi, perenimi
+from opilane
+ORDER BY stip;
+--kahanevas järjekorras - убывание - DESC - suurimast --> väikseni, Z-->A
+select opilaneID, eesnimi, perenimi
+from opilane
+ORDER BY stip;
+
+--kuva eesnimi, mis on sorteeritud kahanevas järjekorras
+select eesnimi
+from opilane
+ORDER BY eesnimi DESC;
+
+--VÕRDLUSED
+-- näita eesnimed mis algavad  A tähega
+SELECT eesnimi, perenimi
+FROM opilane
+WHERE eesnimi LIKE 'A%' OR perenimi LIKE 'A%'
+-- näita õpilased, kellel eesnimed ja perenimed algavad A tähega
+SELECT eesnimi, perenimi
+FROM opilane
+WHERE eesnimi LIKE 'A%' AND perenimi LIKE 'A%'
+
+--näita eesnimed, kellel esimene ja viimane täht on sama
+select eesnimi
+from opilane
+WHERE LEFT(eesnimi, 1)=RIGHT(eesnimi, 1)
+
+-- < - väiksem kui, > - suurem kui, = -võrdne
+--näita õpilase, kelle stip on suurem kui 50 ja väiksem ku i70
+select perenimi, stip
+from opilane
+WHERE stip>50 AND stip<70
+
+--BETWEEN
+select perenimi, stip
+from opilane
+WHERE stip BETWEEN 50 and 70
+ORDER by perenimi
+-- päringus sorteerimine ORDER by kirjutame viimasena
+
+--Agregaarfunktisoonid -COUNT, SUM, AVG, MIN, MAX
+--Leia õpilaste arv
+SELECT count(*) AS 'opilasteArv'
+FROM opilane;
+
+--Leia keskmine stip
+select AVG(stip) as 'keskime stip'
+FROM opilane;
+
+--näita 5 õpilased kõige suurema stipiga
+SELECT top 5 perenimi, stip
+from opilane
+Order by stip
+
+--XAMPP kasutame TOP asamel LIMIT
+--Select perenimi, stip
+--from opilane
+--Order by stip DESC
+--LIMIT 5;
+
+-- komboneerime tingimused
+-- eesnimied sisaldavad T ja stip (60;70)
+select eesnimi, stip
+from opilane
+where eesnimi like '%t%' and  stip between 60 and 70
+
+
+--- ülisandet
+create table 
